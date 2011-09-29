715abdb1daf4b7739c91b40762bea0a25a2669ea
Anthony Williams <hi@antw.me>
2011-09-29 01:36:52 +0100

    Add NODE_PATH.

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,5 +1,8 @@
 # Node installs binaries to /usr/local/share/npm/bin
 PATH="/usr/local/share/npm/bin:$PATH"; export PATH
 
+# Node libraries...
+NODE_PATH="/usr/local/lib/node" ; export NODE_PATH
+
 # Python installs binaries to /usr/local/share/python.
 PATH="/usr/local/share/python:$PATH";  export PATH
