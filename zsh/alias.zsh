3f2ed0bda7ad4a1f86930b5e57aa473af0a2aff9
Anthony Williams <hi@antw.me>
2011-11-11 00:12:03 +0000

    Enable colour "ls" on Ubuntu.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,7 +3,14 @@ alias pgstart='pg_ctl -D /usr/local/var/postgres -s -l /usr/local/var/postgres/r
 alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 alias giti='git'
 
-alias ls="ls -G"
 alias tvnamer="tvnamer --config='$HOME/.tvnamer'"
 
 export LESS="-R -M -S --shift 5"
+
+if [[ -f /etc/lsb-release ]] ; then
+  # Ubuntu
+  alias ls="ls --color"
+else
+  # OS X.
+  alias ls="ls -G"
+fi
